89885952759da3b83b68f804207ce8dec5d6744b
Sahat Yalkabov <sakhat@gmail.com>
2014-11-13 11:36:25 -0800

    Initial Commit

---
+++ yahoo-ios-sdk/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  yahoo-ios-sdk
+//
+//  Created by Sahat Yalkabov on 11/13/14.
+//  Copyright (c) 2014 Sahat Yalkabov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
